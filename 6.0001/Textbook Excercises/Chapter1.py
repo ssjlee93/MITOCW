7ab1aeca32fdfcc534291273b425eb162a537e74
Sung Jae Lee <sungjaelee@sungs-mac-mini.mynetworksettings.com>
2023-05-01 23:33:59 -0400

    finished Chapter 1

---
+++ 6.0001/Textbook Excercises/Chapter1.py
@@ -0,0 +1,19 @@
+# Chapter 1 Finger Exercise
+
+def print_my_answer():
+    print("Driving between my home and Chipotle")
+    print("1. Take the car and exit our lobby")
+    print("2. Turn right onto Executive Drive")
+    print("3. Continue on Executive Drive until you see the first left turn")
+    print("4. Take the left turn")
+    print("5. Continue to the end of the road.")
+    print("6. Turn right")
+    print("7. Continue until you pass Something Something Cleaners")
+    print("8. After you pass the cleaners, turn left.")
+    print("9. Continue straight and turn left near the entrance of the parking lot.")
+    print("10. Pass 3 aisles of parking lot.")
+    print("11. Turn left.")
+    print("12. the destination is straight ahead")
+
+
+print_my_answer()
